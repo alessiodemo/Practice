765ff3e7e0876f1120b7794d699b18fa49cd7936
Alessio Demo <96428793+alessiodemo@users.noreply.github.com>
2025-03-03 19:34:03 +0100

    Add files via upload

---
+++ int main().cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+    int number = 3;
+    int number2 = 2;
+
+    int result;
+    result=number*number2;
+
+    std::cout<<"Il risultato è:"<<result;
+
+    return 0;
+}
